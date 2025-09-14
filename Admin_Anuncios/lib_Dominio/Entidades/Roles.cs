0c55a40e6478e416dda066fef01aa50951ed2206
Pelaez2402 <sebastianpelaez1132851@correo.itm.edu.co>
2025-09-14 13:29:24 -0500

    Empezando el llenado de clases

---
+++ Admin_Anuncios/lib_Dominio/Entidades/Roles.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace lib_Dominio.Entidades
+{
+    public class Roles
+    {
+        public int id_rol { get; set; }
+        public string? nombre { get; set; }
+        public string? descripcion { get; set; }
+
+         //id_rol INT PRIMARY KEY IDENTITY(1,1),
+        //nombre NVARCHAR(50) NOT NULL,
+        //descripcion NVARCHAR(255),
+     //fecha_creacion DATE,
+     //activo BIT default 1 
+    }
+}
---
+++ Admin_Anuncios/lib_Dominio/Entidades/Usuarios.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace lib_Dominio.Entidades
+{
+    public class Usuarios
+    {
+            public int id_usuario { get; set; }
+            public string? nombre{ get; set; }
+            public string? correo { get; set; }
+            public string? contraseña { get; set; }
+            public string? telefono { get; set; }
+            public DateTime fecha_registro { get; set; }
+    
+        }
+}
