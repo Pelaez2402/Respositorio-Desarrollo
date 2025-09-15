c7b314cb31b32a00c0e351c318020be86ee7f87a
Pelaez2402 <sebastianpelaez1132851@correo.itm.edu.co>
2025-09-14 19:44:57 -0500

    Terminadas todas las entidades

---
+++ ADMINISTRADOR_ANUNCIOS/lib_dominio/Entidades/ImagenesAnuncios.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System;
+
+namespace lib_dominio.Entidades
+{
+    public class ImagenesAnuncios
+    {
+        public int ImagenID { get; set; }
+        public string? url { get; set; }
+        public int AnuncioID{get;set;}
+        public Anuncios Anuncios { get; set; } = null!;
+        public string? Titulo { get; set; }
+    
+    }
+}
---
+++ ADMINISTRADOR_ANUNCIOS/lib_dominio/Entidades/PlanesDePublicacion.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace lib_dominio.Entidades
+{
+    public class PlanesDePublicacion
+    {
+        public int PlanID { get; set; }
+        public string? Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Duracion { get; set; }
+        public string? Descripcio { get; set; }
+    }
+}
